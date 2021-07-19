416d2c03577a4a771b4199a453a4f06dc9d2060f
Elias <eliassharafi14422@gmail.com>
2021-07-19 15:11:05 +0430

    add dependency of persianDate and create footer.php

--- index.php
+++ index.php
@@ -190,9 +190,4 @@
 
     </div>
 </div>
-<script src="jquery/jquery-3.5.1.slim.min.js"></script>
-<script src="jquery/bootstrap.bundle.min.js"></script>
 
-
-</body>
-</html>
---
+++ template/footer.php
@@ -0,0 +1,9 @@
+<script src="jquery/jquery-3.5.1.slim.min.js"></script>
+<script src="jquery/bootstrap.bundle.min.js"></script>
+
+<link rel="stylesheet" href="node_modules/persian-datepicker/dist/css/persian-datepicker.css"/>
+<script src="node_modules/jquery/dist/jquery.js"></script>
+<script src="node_modules/persian-date/dist/persian-date.js"></script>
+<script src="node_modules/persian-datepicker/dist/js/persian-datepicker.js"></script>
+</body>
+</html>
